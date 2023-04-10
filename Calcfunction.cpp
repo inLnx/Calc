4f502c0d1050629b15fe5ba9a26334cf3b45f8bc
inLnx <124644075+inLnx@users.noreply.github.com>
2023-04-10 10:00:57 -0500

    Calcfunction.cpp
    The Base of a calculator

---
+++ Calcfunction.cpp
@@ -0,0 +1,56 @@
+#include <iostream>;
+#include <cmath>;
+
+int main ()
+{
+
+  
+int num1 ()
+{
+cout << "Enter First Number" << endl;
+cin >> num1;
+}
+  
+  
+  
+int oper ()
+{
+  cout << "Pick a Operator" << endl;
+  cin >> oper
+  
+}
+  
+ 
+
+int num2 ()
+{
+ cout << "Pick a 2nd number" 
+  cin >> num2
+  
+  
+  
+}
+
+  
+  
+  cout << "num1 oper num2" 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+}
