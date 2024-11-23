6f3414c6a80b4b4764f63e1c124d3c7283f405a9
sterling duncan <Adlaugh@net-shopping.com>
2024-11-22 22:26:15 -0600

    Update Calcfunction.cpp

--- Calcfunction.cpp
+++ Calcfunction.cpp
@@ -1,49 +1,48 @@
 #include <iostream>
 #include <cmath>
-use namespace std
 
-int main ()
-{
+using namespace std;
 
-  
-int num1 ()
-{
-cout << "Enter first number" << endl;
-cin >> num1;
+int main() {
+    int num1, num2;
+    char oper;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> oper;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    double result;
+    switch (oper) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 == 0) {
+                cout << "Error: Division by zero\n";
+                return 1;
+            }
+            result = (double)num1 / num2;
+            break;
+        default:
+            cout << "Invalid operator\n";
+            return 1;
+    }
+
+    cout << num1 << " " << oper << " " << num2 << " = " << result << endl;
+
+    return 0;
 }
-  
-  
-  
-int oper ()
-{
-  cout << "Pick a Operator" << endl;
-  cin >> oper;
-  
-}
-  
- 
-
-int num2 ()
-{
- cout << "Pick a 2nd number"; 
-  cin >> num2;
-  
-  
-  
-}
-
-  
-  
-  cout << ("num1 'oper' num2"); 
-  
-  
-  
-  
-  
-  
-  
-  
-  
   
   
   
