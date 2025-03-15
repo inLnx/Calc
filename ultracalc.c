daf8b7940d885ca5b5be5ffd813c64599e4a3759
sterling duncan <Adlaugh@net-shopping.com>
2025-03-15 19:07:03 +0000

    just committed 1/8 of the C version

---
+++ ultracalc.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+        printf ("UltraCalc")
+        printf("Copyright Scutoid and Inlnx, 2025")
+        // Yes, I copyright my calculator apps 
+           // ends code
+    return 0;
+}
